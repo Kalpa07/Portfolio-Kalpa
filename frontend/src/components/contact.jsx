ef653dcda11c77162027fdc68e888f41a9547a04
Kalpa07 <kalpateli0711@gmail.com>
2025-05-01 20:24:32 +0530

    added responsiveness for contact section

--- frontend/src/components/contact.jsx
+++ frontend/src/components/contact.jsx
@@ -56,31 +56,31 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex flex-row w-full items-center justify-center gap-[60px]">
-      <div className="p-2">
-        <h2 className="text-primary text-6xl w-150">
+    <div className="flex flex-col sm:flex-row w-full items-center justify-center gap-[60px]">
+      <div className="p-5 sm:p-2">
+        <h2 className="text-primary text-4xl sm:text-6xl w-120 sm:w-150">
           Paper planes, pixels, and possibilities!
         </h2>
-        <hr className="w-150 text-sm ml-[20px] mb-5 mt-5 text-text"></hr>
-        <p className=" w-180 text-2xl">
+        <hr className="w-80 sm:w-150 text-sm ml-[20px] mb-5 mt-5 text-text"></hr>
+        <p className=" w-110 sm:w-180 text-xl sm:text-2xl">
           Have an idea flying around in your mind? Let’s catch it together. <br />
           I’m just a message away — say{' '}
-          <span className="italic font-poppins text-3xl bolder">Hi!</span> and
+          <span className="italic font-poppins text-xl sm:text-3xl bolder">Hi!</span> and
           let’s turn your vision into something beautiful.
         </p>
-          <span className='text-xl'>
+          <span className='text-lg sm:text-xl'>
             Or, if you prefer, <a href="mailto:hikalpa07@gmail.com" className='underline'>email me directly!</a>
           </span>
       </div>
       <div className="flex flex-col p-2">
         <form onSubmit={handleSubmit}>
           <div className='flex flex-col'>
-            <div className="flex flex-row w-120 gap-4">
+            <div className="flex flex-row w-100 sm:w-120 gap-4">
               <input
                 type="text"
                 name="name"
                 placeholder="Name"
-                className="input input-ghost w-full input-lg border-0 border-b border-b-gray-700"
+                className="input input-ghost w-50 sm:w-full input-lg border-0 border-b border-b-gray-700"
                 value={formData.name}
                 onChange={handleChange}
               />
@@ -88,7 +88,7 @@ const Contact = () => {
                 type="email"
                 name="email"
                 placeholder="Email Id"
-                className="input input-ghost w-full input-lg border-0 border-b border-b-gray-700"
+                className="input input-ghost w-50 sm:w-full input-lg border-0 border-b border-b-gray-700"
                 value={formData.email}
                 onChange={handleChange}
               />
@@ -97,20 +97,20 @@ const Contact = () => {
               type="text"
               name="subject"
               placeholder="Subject"
-              className="input input-ghost mt-10 w-120 input-lg border-0 border-b border-b-gray-700"
+              className="input input-ghost mt-10 w-100 sm:w-120 input-lg border-0 border-b border-b-gray-700"
               value={formData.subject}
               onChange={handleChange}
             />
             <textarea
               name="message"
-              className="textarea textarea-ghost input-lg w-120 border-0 border-b border-b-gray-700 mt-10"
+              className="textarea textarea-ghost input-lg w-100 sm:w-120 border-0 border-b border-b-gray-700 mt-10"
               placeholder="Message"
               value={formData.message}
               onChange={handleChange}
             ></textarea>
             <button
               type="submit"
-              className="text-text hover:cursor-pointer btn btn-primary hover:scale-105 transform transition-transform duration-300 text-xl hover:border p-2 px-5 rounded-3xl w-40 mt-10"
+              className="text-text hover:cursor-pointer btn btn-primary hover:scale-105 transform transition-transform duration-300 text-lg sm:text-xl hover:border p-2 px-5 rounded-3xl w-20 sm:w-40 mt-10"
               disabled={loading}
             >
               {loading ? 'Sending...' : 'Send'}
