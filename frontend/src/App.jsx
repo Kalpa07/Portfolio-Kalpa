a3b1d4b91620675f2db49176e9b8821447bcd486
Kalpa07 <kalpateli0711@gmail.com>
2025-06-12 03:05:10 +0530

    changes in route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/projects" element={<Projects/>}/>
-      <Route path="/resume" element={<Resume/>}/>
+      <Route path="/resume-Kalpa" element={<Resume/>}/>
     </Routes>
     </div>
   )
