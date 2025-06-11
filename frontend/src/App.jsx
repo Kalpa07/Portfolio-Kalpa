f4464ebff050904557300d62b52c4739682f6cb1
Kalpa07 <kalpateli0711@gmail.com>
2025-06-12 03:00:03 +0530

    added resume route, added iframe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Projects from './pages/Projects';
 import Home from './pages/Home';
 import { Route,Routes } from 'react-router-dom';
 import Loader from "./components/Loader";
+import Resume from './pages/Resume';
 
 
 const App = () => {
@@ -34,6 +35,7 @@ const App = () => {
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/projects" element={<Projects/>}/>
+      <Route path="/resume" element={<Resume/>}/>
     </Routes>
     </div>
   )
---
+++ frontend/src/pages/Resume.jsx
@@ -0,0 +1,17 @@
+
+const Resume = () => {
+
+  return (
+    <>
+    <div className="h-full w-full bg-white dark:bg-black text-black dark:text-white flex flex-col items-center justify-start ">
+      <iframe
+        src="/documents/Kalpa Teli-Resume.pdf"
+        title="Kalpa Teli Resume"
+        className="w-full h-screen "
+        ></iframe>
+     </div>
+    </>
+  );
+};
+
+export default Resume;
